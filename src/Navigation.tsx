2a155937c4b10fd0e84ebd0e9c9228074e2813e4
Théodore van der Rest <theodorevdrest@gmail.com>
2023-08-21 17:27:55 +0200

    home page maquette

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -2,14 +2,14 @@ import React from "react";
 function Navigation() {
     return (
         <>
-            <nav className="c-mainnav">
+            <nav className="c-mainnav o-container">
+                <div><a href="#"><img width="100" src="https://20stm.com/_nuxt/img/20stm_logo.08a6418.png" alt=""/></a></div>
                 <ul className="c-nav-list">
-                    <li><a href="https://20stm.com/" className="c-nav--link">Logo</a></li>
                     <li><a href="/" className="c-nav--link">Home</a></li>
                     <li><a href="/about" className="c-nav--link">About</a></li>
                     <li><a href="/works" className="c-nav--link">Works</a></li>
                     <li><a href="/values" className="c-nav--link">Our values</a></li>
-                    <li><a href="/contacts" className="c-nav--link">Contact</a></li>
+                    <li><a href="/contacts" className="c-nav--link c-contact--button">Contact</a></li>
                 </ul>
             </nav>
         </>
